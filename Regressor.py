1b8593b9f9748564c6ea558db8185a7f4e64ec8c
JayWadekar <jayshine12@gmail.com>
2020-02-23 15:00:42 -0500

    started regressor

--- Regressor.py
+++ Regressor.py
@@ -3,87 +3,103 @@
 #SBATCH --partition=p100_4,p40_4
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:3
-#SBATCH --time=10:00:00
+#SBATCH --time=12:30:00
 #SBATCH --mem=8GB
-#SBATCH --cpus-per-task=22
+#SBATCH --cpus-per-task=20
 #SBATCH -o "/scratch/dsw310/CCA/output/Reg/outReg.log"
 #SBATCH -e "/scratch/dsw310/CCA/output/Reg/errReg.log"
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=dsw310@nyu.edu
 
 #Notes: Look at LR, number of epochs, input net
-#Changed: 
+#Changed: Loss criterion
 
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-import sys
-import time
+from torch.utils.data.dataset import Dataset
+import sys, time
 sys.path.insert(0, '/scratch/dsw310/CCA/functions')
 from uNet import *
-from data_utils import SimuData
 from tools import *
+from data_utils import *
 
-
-#dire='/scratch/dsw310/CCA/trash/TrashOut/'
-#imp=''
-
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 dire='/scratch/dsw310/CCA/output/Reg/'
-imp=''
 
-num_epochs=15
-batch_size=24; num_workers=20
+num_epochs=100
+batch_size=42; num_workers=16
 #eval_frequency=2
 
-loss_train = []
-loss_val = []
-best_val = 1e8
-thres=0.1
+IndList2=np.loadtxt('/scratch/dsw310/CCA/data/extras/IndList/IndList_original.dat'); IndList2=IndList2.astype(int)  
+class SimuData2(Dataset):
+    def __init__(self,lIndex,hIndex,hod=0,aug=0,test=0):
+        self.datafiles = []
+        self.hod=hod
+        self.aug=aug
+        self.test=test
+        for i in np.arange(lIndex,hIndex):
+            self.datafiles+=[IndList2[i]]
+    def __getitem__(self, index):
+        return get_mini_batch(self.datafiles[index],self.hod,self.aug,self.test)
+    def __len__(self):
+        return len(self.datafiles)
 
-net = VanillaUnet(BasicBlock)
-net.cuda()
-net = nn.DataParallel(net)
-net = torch.load('/scratch/dsw310/CCA/Saved/BestModel/Reg/1031.pt')
-criterion = weighted_nn_loss(10.,thres)#Loss_weight=30.
-optimizer = torch.optim.Adam(net.parameters(),lr=1e-5, betas=(0.9, 0.999), eps=1e-08,weight_decay=1e-4)
+best_val = 1e10; thres=0.6
+mask_model = nn.DataParallel(DMUnet(BasicBlock).to(device))
+mask_model.load_state_dict(torch.load('/scratch/dsw310/CCA/Saved/BestModel/Unet/0208_sd.pt'))
+mask_model.eval()
+for param in mask_model.parameters(): param.requires_grad = False
 
+reg_model = nn.DataParallel(RegUnet(BasicBlock).to(device))
+#reg_model.load_state_dict(torch.load('/scratch/dsw310/CCA/Saved/BestModel/Reg/0218.pt'))
+
+#criterion = reg_loss(weight_ratio=5e-3,thres=0.87)
+optimizer = torch.optim.Adam(reg_model.parameters(),lr=1e-8, betas=(0.9, 0.999), eps=1e-08,weight_decay=1e-5)
 start_time = time.time()
 
-TrainSet=SimuData(0,165000)#0,180000
-ValSet=SimuData(165000,180000)#180000,200000
+TrainSet=SimuData2(0,165000,hod=0,aug=1)
+ValSet=SimuData2(165000,180000,hod=0,aug=1)
 TrainLoader=DataLoader(TrainSet, batch_size=batch_size,shuffle=True, num_workers=num_workers)
 ValLoader=DataLoader(ValSet, batch_size=batch_size,shuffle=True, num_workers=num_workers)
+loss_train = []; loss_val = []
 
 for epoch in range(num_epochs):
     print('Epoch {}/{}'.format(epoch, num_epochs - 1))
     print('-' * 10)
-    net.train()
+    reg_model.train()
     for t, data in enumerate(TrainLoader, 0):
         optimizer.zero_grad()
-        Y_pred = net(data[0].cuda(),data[1].cuda())
-        target=data[2].cuda()
-        #mask = (target > thres).float()
-        loss = criterion(Y_pred, target)
-        #loss =(((Y_pred - target).pow(2))*mask).mean()
+        inp=data[0].cuda()
+        mask = (mask_model(inp) > thres)
+        if mask.any()==0: continue
+        Y_pred = (reg_model(inp))[mask]
+        target=(data[1].cuda())[mask]
+        loss =((Y_pred - target).pow(2)*torch.exp((target-thres)*8.)).mean() if mask.any()>0 else 0.
+        #loss = criterion(Y_pred[mask], target[mask])
         loss_train.append(loss.item())
         loss.backward()
         optimizer.step()
-    np.savetxt(dire+'trainReg'+imp+'.dat',loss_train)
+    np.savetxt(dire+'trainReg.dat',loss_train)
     
-    net.eval()
+    reg_model.eval()
     _loss=0
     for t_val, data in enumerate(ValLoader,0):
         with torch.no_grad():
-            Y_pred = net(data[0].cuda(),data[1].cuda())
-            target=data[2].cuda()
-            _loss += criterion(Y_pred, target).item()
+            inp=data[0].cuda()
+            mask = (mask_model(inp) > thres)
+            if mask.any()==0: continue
+            Y_pred = (reg_model(inp))[mask]
+            target=(data[1].cuda())[mask]
+            _loss =(((Y_pred - target).pow(2)*torch.exp((target-thres)*8.)).mean()).item() if mask.any()>0 else 0.
     loss_val.append(_loss/(t_val+1))
-    np.savetxt(dire+'valReg'+imp+'.dat',loss_val)
+    np.savetxt(dire+'valReg.dat',loss_val)
     if( _loss/(t_val+1) < best_val):
         best_val= _loss/(t_val+1)
-        torch.save(net,dire+'BestReg'+imp+'.pt')
+        print('Saving model loss:',best_val)
+        torch.save(reg_model.state_dict(),dire+'BestReg.pt')
     time_elapsed = time.time() - start_time
     print('Time {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
     
--- Regressor_old.py
+++ Regressor_old.py
@@ -3,17 +3,18 @@
 #SBATCH --partition=p100_4,p40_4
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:3
-#SBATCH --time=12:00:00
+#SBATCH --time=10:00:00
 #SBATCH --mem=8GB
 #SBATCH --cpus-per-task=22
-#SBATCH -o "/scratch/dsw310/CCA/output/Unet/outUnet.log"
-#SBATCH -e "/scratch/dsw310/CCA/output/Unet/errUnet.log"
+#SBATCH -o "/scratch/dsw310/CCA/output/Reg/outReg.log"
+#SBATCH -e "/scratch/dsw310/CCA/output/Reg/errReg.log"
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=dsw310@nyu.edu
 
-#Notes: Look at LR, number of epochs, input net, Loss power
+
+# This regressor was implemented for the full resolution Illustris to work alongside a classifier
+#Notes: Look at LR, number of epochs, input net
 #Changed: 
-#Time 49m for 42 Batchsize
 
 import numpy as np
 import torch
@@ -25,35 +26,37 @@ import time
 sys.path.insert(0, '/scratch/dsw310/CCA/functions')
 from uNet import *
 from data_utils import SimuData
+from tools import *
 
 
 #dire='/scratch/dsw310/CCA/trash/TrashOut/'
 #imp=''
 
-dire='/scratch/dsw310/CCA/output/Unet/'
+dire='/scratch/dsw310/CCA/output/Reg/'
 imp=''
 
-num_epochs=20
-batch_size=42; num_workers=16
+num_epochs=15
+batch_size=24; num_workers=20
 #eval_frequency=2
-
-net = DMUnet(BasicBlock)
-net.cuda()
-net = nn.DataParallel(net)
-net = torch.load('/scratch/dsw310/CCA/Saved/BestModel/Unet/0126_2.pt')
-#criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(net.parameters(),lr=5e-7, betas=(0.9, 0.999), eps=1e-08, weight_decay=1e-5)
-
-start_time = time.time()
-
-TrainSet=SimuData(0,174659,hod=0,aug=1)
-ValSet=SimuData(174659,209481,hod=0,aug=1)# Changed according to input file
-TrainLoader=DataLoader(TrainSet, batch_size=batch_size,shuffle=True, num_workers=num_workers)
-ValLoader=DataLoader(ValSet, batch_size=batch_size,shuffle=True, num_workers=num_workers)
 
 loss_train = []
 loss_val = []
 best_val = 1e8
+thres=0.1
+
+net = VanillaUnet(BasicBlock)
+net.cuda()
+net = nn.DataParallel(net)
+net = torch.load('/scratch/dsw310/CCA/Saved/BestModel/Reg/1031.pt')
+criterion = weighted_nn_loss(10.,thres)#Loss_weight=30.
+optimizer = torch.optim.Adam(net.parameters(),lr=1e-5, betas=(0.9, 0.999), eps=1e-08,weight_decay=1e-4)
+
+start_time = time.time()
+
+TrainSet=SimuData(0,165000)#0,180000
+ValSet=SimuData(165000,180000)#180000,200000
+TrainLoader=DataLoader(TrainSet, batch_size=batch_size,shuffle=True, num_workers=num_workers)
+ValLoader=DataLoader(ValSet, batch_size=batch_size,shuffle=True, num_workers=num_workers)
 
 for epoch in range(num_epochs):
     print('Epoch {}/{}'.format(epoch, num_epochs - 1))
@@ -61,27 +64,28 @@ for epoch in range(num_epochs):
     net.train()
     for t, data in enumerate(TrainLoader, 0):
         optimizer.zero_grad()
-        Y_pred = net(data[0].cuda())
-        target=data[1].cuda()
-        loss =((Y_pred - target).pow(2)*torch.exp((target-0.4)*8.)).mean()
+        Y_pred = net(data[0].cuda(),data[1].cuda())
+        target=data[2].cuda()
+        #mask = (target > thres).float()
+        loss = criterion(Y_pred, target)
+        #loss =(((Y_pred - target).pow(2))*mask).mean()
         loss_train.append(loss.item())
         loss.backward()
         optimizer.step()
-    np.savetxt(dire+'trainUnet'+imp+'.dat',loss_train)
+    np.savetxt(dire+'trainReg'+imp+'.dat',loss_train)
     
     net.eval()
     _loss=0
     for t_val, data in enumerate(ValLoader,0):
         with torch.no_grad():
-            Y_pred = net(data[0].cuda())
-            target=data[1].cuda()
-            #_loss += criterion(Y_pred, data[1].cuda()).item()
-            _loss += ((Y_pred - target).pow(2)*torch.exp((target-0.4)*8.)).mean().item()
+            Y_pred = net(data[0].cuda(),data[1].cuda())
+            target=data[2].cuda()
+            _loss += criterion(Y_pred, target).item()
     loss_val.append(_loss/(t_val+1))
-    np.savetxt(dire+'valUnet'+imp+'.dat',loss_val)
+    np.savetxt(dire+'valReg'+imp+'.dat',loss_val)
     if( _loss/(t_val+1) < best_val):
         best_val= _loss/(t_val+1)
-        torch.save(net,dire+'BestUnet'+imp+'.pt')
+        torch.save(net,dire+'BestReg'+imp+'.pt')
     time_elapsed = time.time() - start_time
     print('Time {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
     
